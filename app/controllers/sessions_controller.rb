50ee8944e127620ccc3dd06cac6d5401b9185e32
Patrick Baselier <patrick.baselier@gmail.com>
2019-04-07 08:52:58 +0200

    Simplify

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,20 @@
 class SessionsController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
-  deserializable_resource :user
 
   def create
-    email = params[:user][:email]
+    email = params[:email]
     @user = User.find_or_create_by!(email: email)
     @user.update!(login_token: SecureRandom.urlsafe_base64,
                   login_token_valid_until: 30.minutes.from_now)
 
     SessionsMailer.magic_link(@user).deliver
-    render jsonapi: @user, class: { User: SerializableSession }
+    render status: :created
   end
 
   private
   def record_invalid(error)
-    render jsonapi_errors: error.record.errors, status: :unprocessable_entity
+    json_errors = error.record.errors.messages.map { | k,v | {title: k, detail: v}}
+    render json: { errors: json_errors},
+      status: :unprocessable_entity
   end
 end
